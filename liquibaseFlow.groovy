916ce521a88d98b8bcf89d5c6a5df64ad3f1bc27
Avinash <Avinash99@gmail.com>
2025-08-22 23:24:59 +0530

    vault operation groovy added

---
+++ liquibaseFlow.groovy
@@ -0,0 +1,12 @@
+def appci(){
+    def flowfile = libraryResource('config/liquibase-flowfile.yaml')
+    writeFile file: 'liquibase-flowfile.yaml', text: flowfile
+
+    ansiColor('xterm') {
+        sh """
+            cat liquibase-flowfile.yaml
+            liquibase --defaultsFile=liquibase.properties changelog-file=changelog/changelog.xml flow --flowfile=${flowfile}
+        """
+    }
+
+}
