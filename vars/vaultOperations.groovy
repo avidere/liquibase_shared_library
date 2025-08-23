3ad42dde452c0c80ec4e2e214f49b51b1b966a30
Avinash <Avinash99@gmail.com>
2025-08-23 12:03:24 +0530

    vault operation added

--- vars/vaultOperations.groovy
+++ vars/vaultOperations.groovy
@@ -11,7 +11,7 @@ def generateToken(String namespace) {
           -H "X-Vault-Namespace: ${namespace}" \
           --request POST \
           --data '{\"role_id\": \"${roleID}\", \"secret_id\": \"${secretID}\"}' \
-          ${vaultAddr}/v1/auth/approle/login | jq -r '.auth.client_token' > token.json
+          ${VAULT_ADDR}/v1/auth/approle/login | jq -r '.auth.client_token' > token.json
         """
         def authProps = readJSON file: 'token.json'
         def token = authProps['client_token']
