b6e45e502264d2a66650ab0636f6c7aa86ea803e
Avinash <Avinash99@gmail.com>
2025-08-23 11:51:47 +0530

    vault operation added

--- vars/loadEnvVars.groovy
+++ vars/loadEnvVars.groovy
@@ -1,10 +1,17 @@
 def call(){
+    def propFileContent = libraryResource 'properties/config.properties'
+    def props = readProperties text: propFileContent
+
+    def credsFileContent = libraryResource 'properties/credentials.properties' 
+    def creds = readProperties text: credsFileContent
+
     env.projectKey = params.PROJECT_KEY
     env.repositoryName = params.REPOSITORY_NAME
     env.gitbranch = params.GIT_BRANCH
     env.baseUrl = "https://github.com"
     env.gitUrl = "${env.baseUrl}/${env.projectKey}/${env.repositoryName}.git"
    // env.gitCredentialsId = ['git-credentials']
-   // env.vaultcred = creds['vaultcred']
+   env.vaultcred = creds['vaultcred']
+   env.VAULT_ADDR = props['VAULT_ADDR']
 
 }
