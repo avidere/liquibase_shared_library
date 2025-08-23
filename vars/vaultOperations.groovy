07c46ed49b0d5f21fa56d415541eb33deb340948
Avinash <Avinash99@gmail.com>
2025-08-23 11:29:46 +0530

    vault operation added

--- vars/vaultOperations.groovy
+++ vars/vaultOperations.groovy
@@ -1,13 +1,20 @@
-def call(){
-    bat """
-    curl -s --header \
-    "X-Vault-Namespace: $VAULT_NAMESPACE" \
-    --request POST --data '{
-      "role_id": "78158080-4e46-35f6-e57f-4156a5384daa",
-      "secret_id": "bee25694-600c-dc2b-16ca-fa9353d2e30d"
-    }' \
-     $VAULT_ADDR/v1/auth/approle/login | jq -r '.auth.client_token'
-
-     """
-    return
+def generateToken(String namespace) {
+    withCredentials([
+        usernamePassword(
+            credentialsId: vaultcred,
+            passwordVariable: 'secretID',
+            usernameVariable: 'roleID'
+        )
+    ]) {
+        bat """
+        curl -k \
+          -H "X-Vault-Namespace: ${namespace}" \
+          --request POST \
+          --data '{\"role_id\": \"${roleID}\", \"secret_id\": \"${secretID}\"}' \
+          ${vaultAddr}/v1/auth/approle/login | jq -r '.auth.client_token' > token.json
+        """
+        def authProps = readJSON file: 'token.json'
+        def token = authProps['client_token']
+        return token
+    }
 }
