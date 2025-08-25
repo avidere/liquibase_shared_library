80160d2c71dda75c306b55539e9f0187bf851541
Avinash <Avinash99@gmail.com>
2025-08-25 12:03:50 +0530

    vaut operation added

--- vars/vaultOperations.groovy
+++ vars/vaultOperations.groovy
@@ -1,11 +1,11 @@
 def generateToken(String namespace) {
     withCredentials([
         usernamePassword(credentialsId: 'vaultcred', passwordVariable: 'secretID', usernameVariable: 'roleID')]) {
-        sh """
+        bat """
             curl -k \
             -H "X-Vault-Namespace: ${namespace}" \
             --request POST \
-            --data '{"role_id": "${roleID}", "secret_id": "${secretID}"}' \
+            --data '{"role_id": "bb5323d8-51a1-3cae-a732-f1325e3ad66d", "secret_id": "01d0613a-53f0-cfdc-f621-102f321f7061"}' \
             ${env.VAULT_ADDR}/v1/auth/approle/login | jq -r .auth > token.json
         """
         }
