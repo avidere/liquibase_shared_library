c86712cf88b04851570e2f4f8789d3892b848540
Avinash <Avinash99@gmail.com>
2025-08-29 13:17:47 +0530

    flowfile added

--- vars/liquibaseflowci.groovy
+++
@@ -1,5 +0,0 @@
-def call () {
-    sh """
-        liquibase --defaultsFile=liquibase.properties flow --flowfile=liquibase-ci.flowfile.yaml --output-file=output.txt --log-file-liquibase.log
-        """
-}
