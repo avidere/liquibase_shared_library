6fb8298beb32359bd0144bd7f346278cd6928e00
Avinash <Avinash99@gmail.com>
2025-08-30 15:04:56 +0530

    role id and secret id added

--- vars/vaultOperations.groovy
+++ vars/vaultOperations.groovy
@@ -3,13 +3,13 @@ def generateToken(String namespace) {
         usernamePassword(credentialsId: 'vaultcred', passwordVariable: 'secretID', usernameVariable: 'roleID')]) {
         sh """
             curl -k \
-            -H "X-Vault-Namespace: ${namespace}" \
+            -H "X-Vault-Namespace: admin" \
             --request POST \
             --data '{
                 "role_id": "bb5323d8-51a1-3cae-a732-f1325e3ad66d",
                 "secret_id": "01d0613a-53f0-cfdc-f621-102f321f7061"
             }' \
-            ${env.VAULT_ADDR}/v1/auth/approle/login | jq -r .auth > token.json
+            https://vault-cluster-1-public-vault-399c773d.4213bc99.z1.hashicorp.cloud:8200/v1/auth/approle/login | jq -r .auth > token.json
         """
         }
         def authProps = readJSON file: 'token.json'
