697bbed0ee53f3f728205ba0f9bc46e63c3782b0
Avinash <Avinash99@gmail.com>
2025-08-30 15:19:49 +0530

    secret stored in creds

--- vars/vaultOperations.groovy
+++ vars/vaultOperations.groovy
@@ -1,13 +1,13 @@
 def generateToken(String namespace) {
     withCredentials([
-        usernamePassword(credentialsId: 'vaultcred', password: 'secretID', username: 'roleID')]) {
+        usernamePassword(credentialsId: 'vaultcred', passwordVariable: 'secretID', usernameVariable: 'roleID')]) {
         sh """
             curl -k \
             -H "X-Vault-Namespace: admin" \
             --request POST \
             --data '{
-                "role_id": $roleID,
-                "secret_id": $secretID
+                "role_id": "${roleID}",
+                "secret_id": "${secretID}"
             }' \
             https://vault-cluster-1-public-vault-399c773d.4213bc99.z1.hashicorp.cloud:8200/v1/auth/approle/login | jq -r .auth > token.json
         """
