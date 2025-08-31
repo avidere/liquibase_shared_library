26efea0158acec3ce485078fb121bc6c9976ade7
Avinash <Avinash99@gmail.com>
2025-08-31 20:40:20 +0530

    baseurl updated

--- vars/ServiceNowUpdate.groovy
+++ vars/ServiceNowUpdate.groovy
@@ -1,11 +1,13 @@
 def call() {
     withCredentials([usernamePassword(credentialsId: ServiceNow, passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-            if ( "${REQUEST_NUMBER}".contains('CHG')) {
+            if ("${REQUEST_NUMBER}".contains('CHG')) {
                 table_type = 'change_request'
+            } else if ("${REQUEST_NUMBER}".contains('REQ')) {
+                table_type = 'sc_request'
             } else {
                 table_type = 'sc_req_item'
             }
-            def curlResponse = sh(returnStdout: true, script: "curl -s -k -u ${USERNAME}:'${PASSWORD}' '${SNApi}/table/${table_type}?sysparam_query=number=number=${REQUEST_NUMBER}&sysparam_display_value=true&' | jq -r .result[]")
+            def curlResponse = sh(returnStdout: true, script: "curl -s -k -u ${USERNAME}:'${PASSWORD}' '${SNApi}/table/${table_type}?sysparam_query=number=${REQUEST_NUMBER}&sysparam_display_value=true&' | jq -r .result[]")
 
             def json = readJSON(text: curlResponse)
             sn_request_sys_id = json.sys_id
