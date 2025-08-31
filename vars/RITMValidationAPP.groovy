ededc3889803a1f9d30a539a6247170baebb64d7
Avinash <Avinash99@gmail.com>
2025-08-31 14:16:04 +0530

    baseurl updated

--- vars/RITMValidationAPP.groovy
+++ vars/RITMValidationAPP.groovy
@@ -3,7 +3,7 @@ def call(def ServiceNow, def REQUEST_NUMBER, def SNApi) {
     try {
         println "Proceeding with RITM Validation...."
        // withCredentials([usernamePassword(credentialsId: ServiceNow, passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-            def curlResponse = sh(returnStdout: true, script: "curl -s -k -u admin:A@vinash2412 'https://dev313863.service-now.com/api/now/table/sc_request?sysparam_display_value=true&sysparam_query=number=number=${RITM_NUMBER}' | jq -r .result[]")
+            def curlResponse = sh(returnStdout: true, script: "curl -s -k -u admin:A@vinash2412 'https://dev313863.service-now.com/api/now/table/sc_request?sysparam_display_value=true&sysparam_query=number=number=REQ0010001' | jq -r .result[]")
             
             if (curlResponse) {
                 def json = readJSON(text: curlResponse)
