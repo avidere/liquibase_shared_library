91f4a9e54197ebe52e39d06ba39f31e070c0d30c
Avinash <Avinash99@gmail.com>
2025-08-31 15:42:58 +0530

    baseurl updated

--- vars/createArtifact.groovy
+++ vars/createArtifact.groovy
@@ -1,28 +1,23 @@
 def call() {
     try {
-        //COMMIT_ID=$(git -c ${WORKSPACE} rev-parse --short=11 HEAD)
         sh '''
-            
             echo changelogName=${changelog} >> artifact.properties
             echo branch=${gitBranch} >> artifact.properties
             echo artifactGroup=${groupId} >> artifact.properties
             echo validationFile=${validationFile} >> artifact.properties
 
-            if [[ ${changelog} =~ "xml"]]
-            then
-                scriptPath=\$(cat ${changelog} | grep -o 'path="[^"]*"' | cut -d'"' -f2 | tr -d '"')
-            elif [[ ${changelog} =~ "yaml" ]]
-            then
-                scriptPath=\$(cat ${changelog} | grep -w "path:" | cut -d: -f2 | tr -d " "))")
+            if [[ ${changelog} =~ "xml" ]]; then
+                scriptPath=$(grep -o 'path="[^"]*"' "${changelog}" | cut -d'"' -f2)
+            elif [[ ${changelog} =~ "yaml" ]]; then
+                scriptPath=$(grep -w "path:" "${changelog}" | cut -d: -f2 | tr -d " ")
             fi
 
-            echo \$scriptPath
+            echo $scriptPath
             cp -f liquibase.properties_from_source config/liquibase.properties
-            zip -r ${WORKSPACE}/{artifact}-${BUILD_NUMBER}.zip  ${changelog} config/liquibase.properties artifact.properties config/* \$validationfile
+            zip -r ${WORKSPACE}/${artifact}-${BUILD_NUMBER}.zip ${changelog} config/liquibase.properties artifact.properties config/* $validationFile
         '''
-
     } catch (e) {
-        echo "Error: An exception during zip worspace stage: ${e}"
+        echo "Error: An exception during zip workspace stage: ${e}"
         error("zip workspace failed")
         currentBuild.result = 'FAILURE'
     }
