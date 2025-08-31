811746fc579f39a1629e4ed746ca74cf96e39623
Avinash <Avinash99@gmail.com>
2025-08-31 17:07:00 +0530

    baseurl updated

--- vars/gitTagCreation.groovy
+++ vars/gitTagCreation.groovy
@@ -1,8 +1,8 @@
 def call() {
     try {
         sh '''
-        git -C ${WORKSPACE} tag ${projKey}=${gitRepo}-${COMMIT_ID}-${BUILD_NUMBER}
-        git -C ${WORKSPACE} pish origin ${projKey}=${gitRepo}-${COMMIT_ID}-${BUILD_NUMBER}
+        git -C ${WORKSPACE} tag ${projKey}-${gitRepo}-${COMMIT_ID}-${BUILD_NUMBER}
+        git -C ${WORKSPACE} push origin ${projKey}-${gitRepo}-${COMMIT_ID}-${BUILD_NUMBER}
         '''
     } catch (e) {
         echo "Error: An exception Occured during the Tag creation stage: ${e}"
