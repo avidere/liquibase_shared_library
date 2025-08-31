19fcf79242a4e514bbd59a590918a1af14dd698d
Avinash <Avinash99@gmail.com>
2025-08-31 14:41:38 +0530

    baseurl updated

--- vars/RITMValidationAPP.groovy
+++ vars/RITMValidationAPP.groovy
@@ -13,17 +13,19 @@ def call(def ServiceNow, def REQUEST_NUMBER, def SNApi) {
                 //print curl response of RITM request
                 println("Requested Item: " + json.short_description)
                 println("Request Current State: " + json.request_state)
-                println("RITM Request display Value: " + json.cat_item.display_value)
+                println("Request Approva State: " + json.approval)
+               // println("RITM Request display Value: " + json.cat_item.display_value)
 
                 // print devops-control RITM display_value value for application
-
-                if (json.cat_item.display_value.contains(display_value)) {
+                if (json.short_description.contains(display_value)) {
+                //if (json.cat_item.display_value.contains(display_value)) { its in real request
                     println "RITM is a valid Database Request. Proceeding with state validation."
                     comment = "RITM is a valid Database Request. Proceeding with state validation.\\n\\n"
                   //  sh "echo '[INFO] $comment' >> $successFile"
                  //   sh "echo '[INFO] $comment' >> $failFile"
 
-                    if (json.state == 'Active') {
+                    if (json.state == 'approved') {
+                   // if (json.state == 'Active') {
                         println "RITM is in Active state"
                         comment = "RITM is in Acti state\\n\\n"
                     //    sh "echo '[INFO] $comment' >> $successFile"
