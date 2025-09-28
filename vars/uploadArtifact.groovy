ff52afabc2368f42911fdb6d05206481aee9abc9
Avinash <Avinash99@gmail.com>
2025-09-28 11:02:27 +0530

    snapshot upload groovy added

--- vars/uploadArtifact.groovy
+++ vars/uploadArtifact.groovy
@@ -31,3 +31,22 @@ def artifactupload() {
 
     }
 }
+
+def snapshotupload(){
+            withCredentials([usernamePassword(credentialsId: NexusCreds, passwordVariable: 'PASSWORD', usernameVariable: 'USER')]) {
+            sh(
+                script: """
+                    set -x
+                    RResponse=\$(curl -v -u ${USER}:${PASSWORD} --upload-file ${SCHEMA_NAME}_Snapshot_${ENVIRONMENT}.json ${nexusUrl}/${PROJECT_KEY}/${REPOSITORY_NAME}/Snapshot/${ENVIRONMENT}/${SCHEMA_NAME}_Snapshot_${ENVIRONMENT}.json -s -o /dev/null -w "%{http_code}")
+                    echo "RResponse: \$RResponse"
+                    if [ "\$RResponse" == "201" ]; then
+                        echo "Success"
+                    else
+                        echo " Failed to upload artifact to Nexus"
+                        exit 1;
+                    fi
+                """
+
+            )
+        }
+}
