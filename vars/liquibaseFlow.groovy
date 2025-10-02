fd9ffe72624ce0d049d589d588fa7b23b18aa9a1
Avinash <Avinash99@gmail.com>
2025-10-02 21:02:21 +0530

    dba code added

--- vars/liquibaseFlow.groovy
+++ vars/liquibaseFlow.groovy
@@ -26,10 +26,10 @@ def dba(String flowfile) {
         comment = "Liquibase Execution is Successful for ${REQUEST_TYPE} workflow\\n\\n"
         sh "echo '[INFO] $timestamp ${comment}' >> $successFile"
 
-        comment "$sql_file is deployed successfully on $DB_NAME\\n\\n"
+        comment ="$sql_file is deployed successfully on $DB_NAME\\n\\n"
         sh "echo '[INFO]  ${comment}' >> $successFile"
 
-        if (ACCESS_REQUEST) {
+        if ("${ACCESS_REQUEST}") {
             comment "$ACCESS_TYPE access for $USER_ID on $DB_NAME\\n\\n"
             sh "echo '[INFO] ${comment}' >> $successFile"
         }
