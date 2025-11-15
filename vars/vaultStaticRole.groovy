e2e4fbca9b7ced566e9734f470b1e4fcd188aac8
Avinash <Avinash99@gmail.com>
2025-11-15 23:13:27 +0530

    vault db config file dded

--- vars/vaultStaticRole.groovy
+++ vars/vaultStaticRole.groovy
@@ -1,20 +1,19 @@
 def call() {
 
     sh """
-        curl -sk \
-        -H "X-Vault-Namespace: admin" \
-        -H "X-Vault-Token: $VAULT_TOKEN" \
-        -H "Content-Type: application/json" \
-        --request POST \
-        --data '{
-            "db_name": "503027034_654654373515_aurora_demo-mysql-db_admin",
-            "rotation_statements": [
-                "ALTER USER `admin`@`%` IDENTIFIED BY \"{{password}}\";"
-            ],
-            "username": "admin",
-            "password": "8F8%?YbhSDp?uQOw"
-        }' \
-        $VAULT_ADDR/v1/database/static-roles/aurora_demo-mysql-db_admin
+            curl -sk -H "X-Vault-Namespace: admin" \
+            -H "X-Vault-Token: $VAULT_TOKEN" \
+            -H "Content-Type: application/json" \
+            --request POST \
+            --data '{
+                "db_name": "503027034_654654373515_aurora_demo-mysql-db_admin",
+                "rotation_statements": [
+                    "ALTER USER `admin`@`%` IDENTIFIED BY \"{{password}}\";"
+                ],
+                "username": "admin",
+                "password": "8F8%?YbhSDp?uQOw"
+            }' \
+            $VAULT_ADDR/v1/database/static-roles/aurora_demo-mysql-db_admin
 
     """
 }
