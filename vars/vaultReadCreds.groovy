b465fc5edd5812ebebbfcd46fc6fac8aa2e9b924
Avinash <Avinash99@gmail.com>
2025-11-16 18:21:11 +0530

    vault read cred added

--- vars/vaultReadCreds.groovy
+++ vars/vaultReadCreds.groovy
@@ -1,9 +1,9 @@
 def usernamePassword(){
     sh """
         curl \
-        -H "X-Vault-Token: $VAULT_TOKEN" \
-        -H "X-Vault-Namespace: $namespace" \
-        $VAULT_ADDR/v1/$path | jq -r .data.data > credentials.json
+        -H "X-Vault-Token: ${VAULT_TOKEN}" \
+        -H "X-Vault-Namespace: ${namespace}" \
+        $VAULT_ADDR/v1/${path} | jq -r .data.data > credentials.json
     """
     def credProps = readJSON file 'credentials.json'
     user = credProps['username']
