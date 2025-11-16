06b6a05456babaf41e0dc0be8dc99a64bb057099
Avinash <Avinash99@gmail.com>
2025-11-16 22:40:01 +0530

    oracle plugin

--- vars/vaultDbConfig.groovy
+++ vars/vaultDbConfig.groovy
@@ -1,4 +1,5 @@
 def mysql(){
+    maskPasswords(varMaskRegexes: [[value: master_pass, VAULT_TOKEN]]) {
     sh """
             curl -sk \
             -H "X-Vault-Namespace: $namespace" \
@@ -16,10 +17,12 @@ def mysql(){
                 "max_connection_lifetime": "0s"
             }' \
             $VAULT_ADDR/v1/database/config/${APP_CIID}_${AWS_ACCOUNT}_${DB_TYPE}_${DB_IDENTIFIER}
-"""
+        """
+    }
 }
 
 def oracle(){
+    maskPasswords(varMaskRegexes: [[value: master_pass, VAULT_TOKEN]]) {
         sh """
             curl -sk \
             -H "X-Vault-Namespace: $namespace" \
@@ -37,5 +40,29 @@ def oracle(){
                 "max_connection_lifetime": "0s"
             }' \
             $VAULT_ADDR/v1/database/config/${APP_CIID}_${AWS_ACCOUNT}_${DB_TYPE}_${DB_IDENTIFIER}
-    """
+        """
+    }
+}
+
+def postgresql(){
+    maskPasswords(varMaskRegexes: [[value: master_pass, VAULT_TOKEN]]) {
+        sh """
+            curl -sk \
+            -H "X-Vault-Namespace: $namespace" \
+            -H "X-Vault-Token: $VAULT_TOKEN" \
+            -H "Content-Type: application/json" \
+            --request POST \
+            --data '{
+                "plugin_name": "vault-plugin-database-oracle",
+                "connection_url": "{{username}}:{{password}}@//${GLOBAL_ENDPOINT}:${PORT}/${DB_NAME}",
+                "username": "${master_user}",
+                "password": "${master_pass}",
+                "allowed_roles":"${APP_CIID}_${AWS_ACCOUNT}_${DB_TYPE}_${DB_IDENTIFIER}_liquibase_deploy_role, ${APP_CIID}_${AWS_ACCOUNT}_${DB_TYPE}_${DB_IDENTIFIER}_liquibase_dare_role, ${APP_CIID}_${AWS_ACCOUNT}_${DB_TYPE}_${DB_IDENTIFIER}_liquibase_dba_role",
+                "max_open_connections": 4,
+                "max_idle_connections": 0,
+                "max_connection_lifetime": "0s"
+            }' \
+            $VAULT_ADDR/v1/database/config/${APP_CIID}_${AWS_ACCOUNT}_${DB_TYPE}_${DB_IDENTIFIER}
+        """
+    }
 }
--- vars/vaultReadCreds.groovy
+++ vars/vaultReadCreds.groovy
@@ -1,14 +1,16 @@
 def usernamePassword(){
-    sh """
-        curl \
-        -H "X-Vault-Token: ${VAULT_TOKEN}" \
-        -H "X-Vault-Namespace: ${namespace}" \
-        $VAULT_ADDR/v1/${VaultPath} | jq -r .data.data > credentials.json
-    """
-    def credProps = readJSON file: 'credentials.json'
-    
-    user = credProps['username']
-    pass = credProps['password']
+    maskPasswords(varMaskRegexes: [[value: master_pass, VAULT_TOKEN]]) {
+        sh """
+            curl \
+            -H "X-Vault-Token: ${VAULT_TOKEN}" \
+            -H "X-Vault-Namespace: ${namespace}" \
+            $VAULT_ADDR/v1/${VaultPath} | jq -r .data.data > credentials.json
+        """
+        def credProps = readJSON file: 'credentials.json'
+        
+        user = credProps['username']
+        pass = credProps['password']
 
-    return [user, pass]
+        return [user, pass]
+    }
 }
