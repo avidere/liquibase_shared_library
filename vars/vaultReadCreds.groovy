ab4538b92978200642db9e5583a6cce7ffec66a9
Avinash <Avinash99@gmail.com>
2025-11-16 16:58:19 +0530

    db config and role created

---
+++ vars/vaultReadCreds.groovy
@@ -0,0 +1,13 @@
+def usernamePassword(){
+    sh """
+        curl \
+        -H "X-Vault-Token: $VAULT_TOKEN" \
+        -H "X-Vault-Namespace: $namespace" \
+        $VAULT_ADDR/v1/$path | jq -r .data.data > credentials.json
+    """
+    def credProps = readJSON file 'credentials.json'
+    user = credProps['username']
+    pass = credProps['password']
+
+    return [user, pass]
+}
